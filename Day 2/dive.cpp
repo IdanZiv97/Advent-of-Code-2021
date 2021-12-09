cf185839c4d2f57d4f19967811a94e1cc3242de1
IdanZiv97 <idanziv7@gmail.com>
2021-12-09 22:14:42 +0200

    Day 2: puzzle 2 solution

---
+++ Day 2/dive.cpp
@@ -0,0 +1,52 @@
+#include <fstream>
+#include <set>
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class dive
+{
+private:
+    int _position[2] = {0, 0};
+    vector<pair<string, int > > _instructions;
+public:
+    //constructor
+    dive(const char* fileName) {
+        vector<pair<string, int> > buffer;
+        fstream data(fileName);
+        // variables to hold the instructions data
+        string instruction;
+        int value;
+        while(data >> instruction >> value) {
+            pair<string, int> temp(instruction, value);
+            buffer.push_back(temp);
+        }
+        _instructions = buffer;
+    }
+    int calculateDivePosition();
+};
+
+int dive::calculateDivePosition() {
+    for(pair<string, int> it : _instructions) {
+        string step = it.first;
+        if (step == "forward") {
+            _position[0] += it.second;
+        } else if (step == "up") {
+            _position[1] -= it.second;
+        } else if (step == "down") { // step == "down"
+            _position[1] += it.second;
+        }
+    }
+    // calculate the product of horizontal and depth
+    return _position[0] * _position [1];
+}
+
+int main()
+{
+    dive diver("directionsInput.txt");
+    cout << diver.calculateDivePosition() << endl;
+    return 0;
+}
+
